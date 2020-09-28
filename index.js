1866bf15441acaab96945cd597f69046b30d584f
Zachery Sheley <zacherysheley@protonmail.com>
2020-09-28 10:06:37 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,18 @@
+function myFunction(imgs) {
+  // Get the expanded image
+  var expandImg = document.getElementById("expandedImg");
+  // Get the image text
+  var imgText = document.getElementById("imgtext");
+  // Use the same src in the expanded image as the image being clicked on from the grid
+  expandImg.src = imgs.src;
+  // Use the value of the alt attribute of the clickable image as text inside the expanded image
+  imgText.innerHTML = imgs.alt;
+  // Show the container element (hidden with CSS)
+  expandImg.parentElement.style.display = "block";
+  //test favicon
+  var link = document.querySelector("link[rel*='icon']") || document.createElement('link');
+    link.type = 'image/png';
+    link.rel = 'shortcut icon';
+    link.href = 'favicon.png';
+    document.getElementsByTagName('head')[0].appendChild(link);
+}
